517d7eb52818422fb7ed1a66eed13684aff7fb92
rhann7 <raihandityam09@gmail.com>
2026-03-05 22:06:03 +0700

    feat: load migrations from subdirectories

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -10,32 +10,24 @@ use Illuminate\Validation\Rules\Password;
 
 class AppServiceProvider extends ServiceProvider
 {
-    /**
-     * Register any application services.
-     */
     public function register(): void
     {
         //
     }
 
-    /**
-     * Bootstrap any application services.
-     */
     public function boot(): void
     {
         $this->configureDefaults();
     }
 
-    /**
-     * Configure default behaviors for production-ready applications.
-     */
     protected function configureDefaults(): void
     {
+        $migrations = glob(database_path('/migrations/*'), GLOB_ONLYDIR);
+        foreach ($migrations as $migration) $this->loadMigrationsFrom($migration);
+
         Date::use(CarbonImmutable::class);
 
-        DB::prohibitDestructiveCommands(
-            app()->isProduction(),
-        );
+        DB::prohibitDestructiveCommands(app()->isProduction());
 
         Password::defaults(fn (): ?Password => app()->isProduction()
             ? Password::min(12)
