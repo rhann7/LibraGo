d8d4676f171e616d70801645f67d4063269b86fc
rhann7 <raihandityam09@gmail.com>
2026-03-12 17:14:36 +0700

    feat: add books resource route

--- routes/books.php
+++ routes/books.php
@@ -1,9 +1,13 @@
 <?php
 
 use App\Http\Controllers\Books\BookCategoryController;
+use App\Http\Controllers\Books\BookController;
 use Illuminate\Support\Facades\Route;
 
 Route::middleware(['auth', 'verified'])->prefix('books')->name('books.')->group(function () {
     Route::resource('categories', BookCategoryController::class)
+        ->only(['index', 'store', 'update', 'destroy']);
+    
+    Route::resource('', BookController::class)
         ->only(['index', 'store', 'update', 'destroy']);
 });
