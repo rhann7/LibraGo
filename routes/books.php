2878e97a081028f7aa1b198c9f71319cf08e7887
rhann7 <raihandityam09@gmail.com>
2026-03-12 22:28:07 +0700

    fix: separate books resource route for proper model binding

--- routes/books.php
+++ routes/books.php
@@ -4,10 +4,12 @@ use App\Http\Controllers\Books\BookCategoryController;
 use App\Http\Controllers\Books\BookController;
 use Illuminate\Support\Facades\Route;
 
-Route::middleware(['auth', 'verified'])->prefix('books')->name('books.')->group(function () {
-    Route::resource('categories', BookCategoryController::class)
+Route::middleware(['auth', 'verified'])->group(function () {
+    Route::resource('books', BookController::class)
         ->only(['index', 'store', 'update', 'destroy']);
     
-    Route::resource('', BookController::class)
-        ->only(['index', 'store', 'update', 'destroy']);
+    Route::prefix('books')->name('books.')->group(function () {
+        Route::resource('categories', BookCategoryController::class)
+            ->only(['index', 'store', 'update', 'destroy']);
+    });
 });
