fd099923c5deee48f3684c9bf4d170201c143cab
rhann7 <raihandityam09@gmail.com>
2026-03-19 23:19:13 +0700

    fix: reorder routes to prevent model binding conflict

--- routes/books.php
+++ routes/books.php
@@ -5,11 +5,11 @@ use App\Http\Controllers\Books\BookController;
 use Illuminate\Support\Facades\Route;
 
 Route::middleware(['auth', 'verified'])->group(function () {
-    Route::resource('books', BookController::class)
-        ->only(['index', 'store', 'update', 'destroy']);
-    
     Route::prefix('books')->name('books.')->group(function () {
         Route::resource('categories', BookCategoryController::class)
             ->only(['index', 'store', 'update', 'destroy']);
     });
+
+    Route::resource('books', BookController::class)
+        ->only(['index', 'store', 'update', 'show', 'destroy']);
 });
