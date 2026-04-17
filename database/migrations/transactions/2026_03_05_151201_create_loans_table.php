29f9cee02083f99ce93508cf15b5de658d69fe87
rhann7 <raihandityam09@gmail.com>
2026-04-17 16:17:40 +0700

    fix: add nullable() to some columns in loans table

--- database/migrations/transactions/2026_03_05_151201_create_loans_table.php
+++ database/migrations/transactions/2026_03_05_151201_create_loans_table.php
@@ -13,8 +13,8 @@ return new class extends Migration
             $table->foreignId('loan_request_id')->constrained()->cascadeOnDelete();
             $table->foreignId('loan_token_id')->constrained()->cascadeOnDelete();
             $table->enum('status', ['active', 'returned'])->default('active');
-            $table->timestamp('borrowed_at');
-            $table->timestamp('due_date');
+            $table->timestamp('borrowed_at')->nullable();
+            $table->timestamp('due_date')->nullable();
             $table->timestamp('returned_at')->nullable();
             $table->timestamps();
         });
