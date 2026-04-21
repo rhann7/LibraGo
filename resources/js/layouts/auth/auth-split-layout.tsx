24da518aaa9180fc79abc9cd7ddbb045b2de0c38
rhann7 <raihandityam09@gmail.com>
2026-04-21 16:26:58 +0700

    feat: update AuthSplitLayout to enhance layout and improve logo display

--- resources/js/layouts/auth/auth-split-layout.tsx
+++ resources/js/layouts/auth/auth-split-layout.tsx
@@ -7,17 +7,20 @@ export default function AuthSplitLayout({ children, title, description }: AuthLa
     const { name } = usePage().props;
 
     return (
-        <div className="relative grid h-dvh flex-col items-center justify-center px-8 sm:px-0 lg:max-w-none lg:grid-cols-2 lg:px-0">
-            <div className="relative hidden h-full flex-col bg-muted p-10 text-white lg:flex dark:border-r">
-                <div className="absolute inset-0 bg-zinc-900" />
-                <Link href={route('home')} className="relative z-20 flex items-center text-lg font-medium">
-                    <AppLogoIcon className="mr-2 size-8 fill-current text-white" /> {name}
+        <div className="relative grid h-dvh flex-col items-center justify-center px-8 sm:px-0 lg:max-w-none lg:grid-cols-[70%_30%] lg:px-0">
+            <div className="relative hidden h-full flex-col p-10 text-white lg:flex dark:border-r">
+                <img src="/storage/img/tb2.jpg" alt="LibraGo" className="absolute inset-0 h-full w-full object-cover" />
+                <div className="absolute inset-0 bg-black/50" />
+                <Link href={route('welcome')} className="relative z-20 flex items-center gap-2 font-semibold text-lg">
+                    <AppLogoIcon className="size-6 fill-current text-white" />
+                    {name}
                 </Link>
             </div>
             <div className="w-full lg:p-8">
                 <div className="mx-auto flex w-full flex-col justify-center space-y-6 sm:w-87.5">
-                    <Link href={route('home')} className="relative z-20 flex items-center justify-center lg:hidden">
-                        <AppLogoIcon className="h-10 fill-current text-black sm:h-12" />
+                    <Link href={route('welcome')} className="relative z-20 flex items-center justify-center gap-2 font-semibold text-lg lg:hidden">
+                        <AppLogoIcon className="size-6 fill-current" />
+                        {name}
                     </Link>
                     <div className="flex flex-col items-start gap-2 text-left sm:items-center sm:text-center">
                         <h1 className="text-xl font-medium">{title}</h1>
