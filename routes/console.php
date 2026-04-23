82baf3668a446a12473c021218f8f867ca34d79c
rhann7 <raihandityam09@gmail.com>
2026-04-23 13:42:27 +0700

    fix: update overdue loan command frequency to every second

--- routes/console.php
+++ routes/console.php
@@ -8,4 +8,4 @@ Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->purpose('Display an inspiring quote');
 
-Schedule::command('loans:update-overdue')->everyThirtySeconds();
+Schedule::command('loans:update-overdue')->everySecond();
