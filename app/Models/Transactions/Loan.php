bba538f14830b351b96851a18a3ccc54c26d5c73
rhann7 <raihandityam09@gmail.com>
2026-04-23 13:41:17 +0700

    feat: update Loan model to change fine relationship from hasOne to hasMany

--- app/Models/Transactions/Loan.php
+++ app/Models/Transactions/Loan.php
@@ -14,7 +14,7 @@ class Loan extends Model
 
     public function loanRequest() { return $this->belongsTo(LoanRequest::class); }
     public function token() { return $this->belongsTo(LoanToken::class, 'loan_token_id'); }
-    public function fine() { return $this->hasOne(Fine::class); }
+    public function fines() { return $this->hasMany(Fine::class); }
 
     public function scopeActive($query) { return $query->where('status', 'active'); }
     public function scopeOverdue($query) { return $query->where('status', 'overdue'); }
