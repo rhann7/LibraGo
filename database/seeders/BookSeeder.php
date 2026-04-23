0ced1558cc71744432b52914902a9383780eb046
rhann7 <raihandityam09@gmail.com>
2026-04-23 13:40:25 +0700

    feat: add price and synopsis fields to book seeder for enhanced book details

--- database/seeders/BookSeeder.php
+++ database/seeders/BookSeeder.php
@@ -37,6 +37,7 @@ class BookSeeder extends Seeder
                 'book_category_id' => $categories[array_rand($categories)],
                 'slug'             => Str::slug($book['title']),
                 'isbn'             => $isbn,
+                'price'            => rand(50000, 300000),
                 'synopsis'         => 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sit voluptates aliquam eum nulla? 
                                        Nostrum quia veniam aut explicabo iusto placeat accusantium unde eos pariatur. 
                                        Deleniti deserunt sequi ad omnis odit aliquam, quaerat officiis temporibus eos tenetur voluptatem, vero soluta debitis?',
